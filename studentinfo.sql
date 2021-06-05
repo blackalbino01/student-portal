c1ef55e30aee37484247c8ec5903bfb50eaa7d04
Hussaini Muhammad Auwal <hauwal4969@gmail.com>
2021-06-05 17:40:41 +0100

    update

---
+++ studentinfo.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Jun 05, 2021 at 06:19 PM
+-- Server version: 10.4.16-MariaDB
+-- PHP Version: 7.4.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `student-portal`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `studentinfo`
+--
+
+CREATE TABLE `studentinfo` (
+  `id` int(11) NOT NULL,
+  `uploadimg` varchar(255) NOT NULL,
+  `fname` varchar(255) NOT NULL,
+  `mname` varchar(255) DEFAULT NULL,
+  `lname` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `birthday` varchar(225) NOT NULL,
+  `gender` varchar(255) NOT NULL,
+  `phone` varchar(255) NOT NULL,
+  `address` varchar(225) NOT NULL,
+  `state` varchar(255) NOT NULL,
+  `localgovt` varchar(255) NOT NULL,
+  `nextofkin` varchar(255) NOT NULL,
+  `jambscore` varchar(255) NOT NULL,
+  `admstatus` varchar(255) NOT NULL DEFAULT 'Undecided'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `studentinfo`
+--
+ALTER TABLE `studentinfo`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `studentinfo`
+--
+ALTER TABLE `studentinfo`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
